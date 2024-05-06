664e8d21b37d555b9a75eda2daea353114bd9db2
Amira Fareh <amirafareh137@hotmail.com>
2024-05-06 18:16:12 +1200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import Dictionary from "./Dictionary"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
+
       <header className="App-header">
        <h1>Dictionary</h1>
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer>
+        <small>Coded by Amira Fareh</small>
+      </footer>
+      </div>
     </div>
   );
 }
